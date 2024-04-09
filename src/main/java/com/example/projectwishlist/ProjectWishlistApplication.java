9f27cccd6dda47f3d8b5189cf6c34fc2583643a5
LasseFoxy <lafo0001@stud.kea.dk>
2024-04-09 15:24:59 +0200

    Initial commit

---
+++ src/main/java/com/example/projectwishlist/ProjectWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectwishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectwishlist/ProjectWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
