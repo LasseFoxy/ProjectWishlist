e271d868591aec4216a5d66edd4eb754a6d4af1d
LasseFoxy <lafo0001@stud.kea.dk>
2024-04-12 12:19:05 +0200

    fjernet metode der er udkommenteret

--- src/main/java/com/example/projectwishlist/controller/WishlistController.java
+++ src/main/java/com/example/projectwishlist/controller/WishlistController.java
@@ -58,23 +58,6 @@ public class WishlistController {
         }
     }
 
-   /* @PostMapping("/wishlist/wishlistItems")
-    public String addItemToWishlist(@ModelAttribute Item item, HttpSession session) {
-        User loggedInUser = (User) session.getAttribute("loggedInUser");
-        Wishlist selectedWishlist = (Wishlist) session.getAttribute("selectedWishlist");
-
-        if (loggedInUser != null && selectedWishlist != null) {
-            if (selectedWishlist.getUserId() == loggedInUser.getUserId()) {
-                wishlistService.addItemToWishlist(selectedWishlist, item);
-                // Muligvis opdater session med den opdaterede wishlist efter tilføjelse af item
-                session.setAttribute("selectedWishlist", selectedWishlist);
-                return "redirect:/wishlist";
-            }
-        }
-        return "redirect:/user/login";
-    }
-    */
-
     @GetMapping("/wishlist/share/{wishlist_id}")
     public String shareWishlist(@PathVariable int wishlist_id, Model model, HttpSession session){
         User loggedInUser = (User) session.getAttribute("loggedInUser");
