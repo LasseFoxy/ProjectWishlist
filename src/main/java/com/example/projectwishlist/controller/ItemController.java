3d8cf88b1148d4b77742c55683fbd1fedb6bf590
DCB <147394907+DCB001@users.noreply.github.com>
2024-04-14 00:05:18 +0200

    Html/css/controller - virker! :D

--- src/main/java/com/example/projectwishlist/controller/ItemController.java
+++ src/main/java/com/example/projectwishlist/controller/ItemController.java
@@ -1,7 +1,10 @@
 package com.example.projectwishlist.controller;
 
 import com.example.projectwishlist.model.Item;
+import com.example.projectwishlist.model.User;
+import com.example.projectwishlist.model.Wishlist;
 import com.example.projectwishlist.service.ItemService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,20 +14,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ItemController {
+
   @Autowired
   private ItemService itemService;
 
-  @GetMapping("/Ønskelisten")
-  public String showItem(Model model) {
-    model.addAttribute("item", new Item());
-    return "addItem";
+
+  @GetMapping("/wishlist/addItem")
+  public String showAddItemForm(Model model, HttpSession session) {
+    User loggedInUser = (User) session.getAttribute("loggedInUser");
+    Wishlist selectedWishlist = (Wishlist) session.getAttribute("selectedWishlist");
+    if (loggedInUser != null) {
+      if (selectedWishlist != null) {
+        model.addAttribute("wishlistId", selectedWishlist.getWishlistId());
+        model.addAttribute("item", new Item());
+        return "addItem";
+      } else {
+        return "redirect:/welcome";
+      }
+    } else {
+      return "redirect:/user/login";
+    }
   }
+
 
   @PostMapping("/saveItem")
-  public String saveItem(@ModelAttribute("item") Item item) {
-    // Gemmer det indsendte vareobjekt i databasen
-    itemService.saveItem(item);
-    return "redirect:/addItem"; // Omdirigerer tilbage til addItem-formularen
-  }
+  public String saveItem(@ModelAttribute("item") Item item, HttpSession session) {
+    User loggedInUser = (User) session.getAttribute("loggedInUser");
+    Wishlist selectedWishlist = (Wishlist) session.getAttribute("selectedWishlist");
 
+    if (selectedWishlist != null && loggedInUser != null) {
+      item.setItemWishlistId(selectedWishlist.getWishlistId());
+      itemService.saveItem(item);
+      return "redirect:/wishlist/wishlistItems";
+    }
+    return "redirect:/welcome";
+  }
 }
